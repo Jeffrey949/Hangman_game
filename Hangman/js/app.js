a49564e08610f2cb28228f0c873383f331a75522
Jeffrey Boateng <Jeffrey.boateng94@gmail.com>
2023-06-04 00:57:07 +0200

    Erste Variablen initialisiert und Spielstart logik implementiert

--- Hangman/js/app.js
+++ Hangman/js/app.js
@@ -1,1 +1,30 @@
+function gameOn() {
+  let zufall, le, eingabe, counter, correct_word;
 
+  function initVaris() {
+    le = wörter.length;
+    zufall = Math.floor(Math.random() * le);
+    eingabe = "";
+    counter = 0;
+    correct_word = wörter[zufall];
+  }
+
+
+  function loadGameField() {
+    let char;
+    for (let i = 0; i < correct_word.length; i++) {
+      char = create("p")
+      char.innerHTML = "_"
+      char.id = i
+      el("#word-section").append(char);
+    }
+  }
+
+
+
+  initVaris();
+  loadGameField();
+
+}
+
+gameOn();
