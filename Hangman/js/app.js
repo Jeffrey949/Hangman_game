9d331952ec1ba35dd48cf3e963e0f658c9eb0847
Jeffrey Boateng <Jeffrey.boateng94@gmail.com>
2023-06-04 04:19:48 +0200

    Implementierung der Logik um korrekte & nicht korrekte Buchstaben anzuzeigen

--- Hangman/js/app.js
+++ Hangman/js/app.js
@@ -1,29 +1,81 @@
 function gameOn() {
-  let zufall, le, eingabe, counter, correct_word;
+  let zufall, le, eingabe, counter, correct_word, charIndex;
 
   function initVaris() {
     le = wörter.length;
     zufall = Math.floor(Math.random() * le);
-    eingabe = "";
+    eingabe = "e";
     counter = 0;
-    correct_word = wörter[zufall];
+    correct_word = wörter[zufall].toUpperCase();
+    charIndex = [];
   }
 
 
   function loadGameField() {
     let char;
+    // Unterstriche abhängig der Wortlänge erstellen
     for (let i = 0; i < correct_word.length; i++) {
       char = create("p")
       char.innerHTML = "_"
       char.id = i
-      el("#word-section").append(char);
+      el('#word-section').append(char);
     }
   }
 
 
+  function keyDown(e) {
+    // Speichern des eigegebenen Buchstaben (Uppercase)
+    eingabe = e.key.toUpperCase();
+    checkInput();
+    // showChar();
 
+  }
+
+  function checkInput() {
+    let char;
+    // Fall für einen korrekten Buchstaben
+    if (correct_word.includes(eingabe)) {
+      for (let i = 0; i < correct_word.length; i++) {
+        // Buchstaben prüfen und die zutreffende indizes in ein array pushen
+        if (correct_word[i] === eingabe) {
+          charIndex.push(i);
+        }
+      }
+      // Platzhalter mit zutreffenden Buchstaben ersetzen
+      for (let i = 0; i < charIndex.length; i++) {
+        document.getElementById(charIndex[i]).innerText = correct_word[charIndex[i]];
+        // el(`#${charIndex[i]}`).innerText = correct_word[charIndex[i]];
+      }
+      // Fall für falsche Eingabe
+    } else {
+      // Fehler Counter wird erhöht und falsche Buchstabe im Feld hinterlegt
+      counter += 1;
+      char = create("li");
+      char.innerText = eingabe;
+      el('#kasten-buchstaben ul').append(char);
+    }
+  }
+
+  // function showChar() {
+  //   for (let i = 0; i < charIndex.length; i++) {
+  //     document.getElementById(charIndex[i]).innerText = correct_word[charIndex[i]];
+  //     // el(`#${charIndex[i]}`).innerText = correct_word[charIndex[i]];
+  //   }
+  // }
+
+
+  function keyUp(e) {
+
+  }
+
+
+  document.addEventListener("keydown", keyDown)
+  document.addEventListener("keyup", keyUp)
   initVaris();
   loadGameField();
+  // checkInput();
+  console.log(correct_word);
+
 
 }
 
